d40b6c45af6fda644407b21e8949afa4fe2717fe
p-mansi <mgpadia@gmail.com>
2026-02-17 13:53:21 +0530

    initial commit

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 124563
+let accountEmail = "mansi@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jetpur"
+let accountState //undefined variable
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+
+    Const - we can not change the value of const variable
+    (accountId = 2) not allowed
+
+    Prefer not to use var
+    because of issue in block scope & functional scope
+
+*/
